6c1ecd8e8aba5280cde713b507cf0c5419f39ea7
Florent Messa <florent.messa@gmail.com>
2012-11-23 18:27:51 +0100

    Add setup.py

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
 django-online
 =============
 
-Standalone application to know if a django user is online or not
+Standalone Django application to know if a django user is online or not.
---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+version = __import__('online').__version__
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(root, 'README.rst')) as f:
+    README = f.read()
+
+setup(
+    name='django-online',
+    version=version,
+    description='Standalone Django application to know if a django user is online or not.',
+    long_description=README,
+    author='Florent Messa',
+    author_email='florent.messa@gmail.com',
+    url='http://github.com/thoas/django-online',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ]
+)
