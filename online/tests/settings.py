d2cdcac5ce958157c91867c29dc1eb94a23b4cf6
Florent Messa <florent.messa@gmail.com>
2012-11-23 19:06:12 +0100

    Add simple exists tests

--- online/tests/settings.py
+++ online/tests/settings.py
@@ -31,5 +31,10 @@ TEMPLATE_DIRS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'online.middleware.OnlineMiddleware',
 )
